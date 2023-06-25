2df9de1279c84bb51fe23c05b1acdc5bc7096ffd
Shadhrush Swaroop <119898772+Shadhrush5@users.noreply.github.com>
2023-06-24 17:51:19 -0700

    Update script.sh

--- HotelReservationAnalysis/script.sh
+++ HotelReservationAnalysis/script.sh
@@ -1,14 +1,14 @@
 python DBMS_DataPreprocessing.py $1 $2
-hadoop fs -mkdir /DBMSShasru/
-hadoop fs -mkdir /DBMSShasru/Input
-hadoop fs -mkdir /DBMSShasru/Input2
-hadoop fs -mkdir /DBMSShasru/Input3
-hadoop fs -put Intermediate_input/input.csv /DBMSShasru/Input
-hadoop fs -put Intermediate_input/input_season.csv /DBMSShasru/Input2
-hadoop fs -put Intermediate_input/input_country.csv /DBMSShasru/Input3
-hadoop jar ProfitableMonth/jarFiles/test1.jar  ProfitableMonthSorted /DBMSShasru/Input /DBMSShasru/Output
-hadoop jar ProfitableMonth/jarFiles/test2.jar  ProfitableMonthSeason /DBMSShasru/Input2 /DBMSShasru/Output2
-hadoop jar ProfitableMonth/jarFiles/test3.jar  ProfitableMonthCountry /DBMSShasru/Input3 /DBMSShasru/Output3
-hadoop fs -copyToLocal /DBMSShasru/Output/part-r-00000 $3/Output1.txt
-hadoop fs -copyToLocal /DBMSShasru/Output2/part-r-00000 $3/Output2.txt
-hadoop fs -copyToLocal /DBMSShasru/Output3/part-r-00000 $3/Output3.txt
+hadoop fs -mkdir /Project/
+hadoop fs -mkdir /Project/Input
+hadoop fs -mkdir /Project/Input2
+hadoop fs -mkdir /Project/Input3
+hadoop fs -put Intermediate_input/input.csv /Project/Input
+hadoop fs -put Intermediate_input/input_season.csv /Project/Input2
+hadoop fs -put Intermediate_input/input_country.csv /Project/Input3
+hadoop jar ProfitableMonth/jarFiles/test1.jar  ProfitableMonthSorted /Project/Input /Project/Output
+hadoop jar ProfitableMonth/jarFiles/test2.jar  ProfitableMonthSeason /Project/Input2 /Project/Output2
+hadoop jar ProfitableMonth/jarFiles/test3.jar  ProfitableMonthCountry /Project/Input3 /Project/Output3
+hadoop fs -copyToLocal /Project/Output/part-r-00000 $3/Output1.txt
+hadoop fs -copyToLocal /Project/Output2/part-r-00000 $3/Output2.txt
+hadoop fs -copyToLocal /Project/Output3/part-r-00000 $3/Output3.txt
